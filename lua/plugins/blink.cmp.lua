273d4579276a644afd94fb797bf9812ed9fddaf9
John Engdal <john@walkbase.com>
2025-02-28 22:28:46 +0200

    Blink autocomplete options

---
+++ lua/plugins/blink.cmp.lua
@@ -0,0 +1,11 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    completion = {
+      menu = { border = "single" },
+      documentation = { window = { border = "single" } },
+      ghost_text = { enabled = false },
+    },
+    signature = { window = { border = "single" } },
+  },
+}
