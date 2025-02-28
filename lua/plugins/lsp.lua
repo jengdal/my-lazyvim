d6c21eced3a2ea5ddffe1bced84d0d9278184d55
John Engdal <john@walkbase.com>
2025-02-28 22:29:11 +0200

    Show pyright errors

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,7 +16,7 @@ return {
           python = {
             analysis = {
               -- Ignore all files for analysis to exclusively use Ruff for linting
-              ignore = { "*" },
+              --ignore = { "*" },
               autoImportCompletions = true,
               autoSearchPaths = true,
             },
