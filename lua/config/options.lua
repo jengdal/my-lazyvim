694cb5fb6ecc5cdea184567d472ea5b1dfe28b5c
John Engdal <john@walkbase.com>
2025-02-28 22:27:42 +0200

    Invert colors with ghostty instead

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,7 @@
 -- Add any additional options here
 
 vim.opt.termguicolors = true
-vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:Cursor/Cursor"
+-- vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:Cursor/Cursor"
 
 vim.g.lazyvim_picker = "snacks"
 
