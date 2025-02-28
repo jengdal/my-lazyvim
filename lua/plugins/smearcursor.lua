6599bf30d87fb58cd11266e9012ac5b0785a8c21
John Engdal <john@walkbase.com>
2025-02-28 22:46:14 +0200

    FAST smear cursor

--- lua/plugins/smearcursor.lua
+++ lua/plugins/smearcursor.lua
@@ -1,19 +1,19 @@
 return {
   "sphamba/smear-cursor.nvim",
   lazy = false,
-  enabled = false,
-  priority = 5000,
+  enabled = true,
+  priority = 1000,
   config = function()
     require("smear_cursor").setup({
-      cursor_color = "#000000",
-      normal_bg = "#ffffff",
+      cursor_color = "#333333",
+      -- normal_bg = "#ffffff",
       -- Smear cursor when switching buffers or windows.
       smear_between_buffers = true,
 
       -- Smear cursor when moving within line or to neighbor lines.
       -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
       smear_between_neighbor_lines = true,
-      min_horizontal_distance_smear = 2,
+      min_horizontal_distance_smear = 5,
       min_vertical_distance_smear = 2,
       -- Draw the smear in buffer space instead of screen space when scrolling
       scroll_buffer_space = true,
@@ -25,6 +25,9 @@ return {
       -- Smear cursor in insert mode.
       -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
       smear_insert_mode = true,
+      stiffness = 0.8,               -- 0.6      [0, 1]
+      trailing_stiffness = 0.7,      -- 0.3      [0, 1]
+      distance_stop_animating = 0.5, -- 0.1      > 0
     })
   end,
   -- opts = {
