412edb3a02577af21693696ad4a7a0897c5e8463
John Engdal <john@walkbase.com>
2025-02-28 09:16:28 +0200

    LSP conf, python

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,41 @@
+return {
+  "neovim/nvim-lspconfig",
+  ---@class PluginLspOpts
+  opts = {
+    ---@type lspconfig.options
+    servers = {
+      -- pyright will be automatically installed with mason and loaded with lspconfig
+      -- pyright = {},
+      pyright = {
+        settings = {
+          pyright = {
+            -- Using Ruff's import organizer
+            disableOrganizeImports = true,
+            openFilesOnly = false,
+          },
+          python = {
+            analysis = {
+              -- Ignore all files for analysis to exclusively use Ruff for linting
+              ignore = { "*" },
+              autoImportCompletions = true,
+              autoSearchPaths = true,
+            },
+          },
+        },
+      },
+      ruff = {
+        on_attach = function(client, bufnr)
+          -- Disable hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+          -- Organize imports via code action on save
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            callback = function()
+              vim.lsp.buf.code_action({ context = { only = { "source.organizeImports" } }, apply = true })
+            end,
+            buffer = bufnr,
+          })
+        end,
+      },
+    },
+  },
+}
