0a6eb71dc5bb0f633a376981a9088d16b8ddebdf
John Engdal <john@walkbase.com>
2025-03-02 19:14:35 +0200

    Treesitter language injections for some languages by comment annotations

---
+++ lua/plugins/tree-sitter-language-injection.lua
@@ -0,0 +1,6 @@
+-- WARNING: This plugin may overwrite any custom injection.scm files you may have in nvim/after/queries/
+return {
+  "dariuscorvus/tree-sitter-language-injection.nvim",
+  enabled = true,
+  opts = {},
+}
