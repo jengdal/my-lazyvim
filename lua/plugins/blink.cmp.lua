4489e5f6d0ee7fdc09df0c31f6c04fd1060e6bd0
John Engdal <john@walkbase.com>
2025-03-03 21:48:29 +0200

    Added this for some ai thing

--- lua/plugins/blink.cmp.lua
+++ lua/plugins/blink.cmp.lua
@@ -5,6 +5,7 @@ return {
       menu = { border = "single" },
       documentation = { window = { border = "single" } },
       ghost_text = { enabled = false },
+      trigger = { prefetch_on_insert = false },
     },
     signature = { window = { border = "single" } },
   },
