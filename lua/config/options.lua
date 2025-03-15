2e68f2ed928591ade37429aff5a559662ce3e92a
John Engdal <john@walkbase.com>
2025-03-15 10:32:47 +0200

    some tabstop things I tried

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
 vim.opt.termguicolors = true
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 8
 -- vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:Cursor/Cursor"
 
 vim.g.lazyvim_picker = "snacks"
