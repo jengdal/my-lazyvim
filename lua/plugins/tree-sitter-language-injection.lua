0197b2297f88599dc0ecb55201296441a9d2cffd
John Engdal <john@walkbase.com>
2025-03-16 09:43:41 +0200

    disable the treesitter injection generator

--- lua/plugins/tree-sitter-language-injection.lua
+++ lua/plugins/tree-sitter-language-injection.lua
@@ -1,6 +1,7 @@
 -- WARNING: This plugin may overwrite any custom injection.scm files you may have in nvim/after/queries/
 return {
   "dariuscorvus/tree-sitter-language-injection.nvim",
-  enabled = true,
+  -- Disable this after the first run, see the warning above.
+  enabled = false,
   opts = {},
 }
