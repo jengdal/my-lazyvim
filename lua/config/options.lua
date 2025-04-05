ed679c5e565f0669c1c6af2b1c6dad472ff49629
John Engdal <john@walkbase.com>
2025-04-05 17:49:52 +0300

    Neovide stuff

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -15,3 +15,13 @@ vim.g.lazyvim_prettier_needs_config = true
 vim.o.background = "light"
 
 vim.g.lazyvim_python_lsp = "basedpyright"
+
+-- if vim.g.neovide == true
+--   vim.keymap.set({ "n", "x" }, "<C-S-C>", '"+y', { desc = "Copy system clipboard" })
+--   vim.keymap.set({ "n", "x" }, "<C-S-V>", '"+p', { desc = "Paste system clipboard" })
+-- end
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("!", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<D-v>", "<C-R>+", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<D-v>", "<C-R>+", { noremap = true, silent = true })
