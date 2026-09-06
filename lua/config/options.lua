d2b9a4873488e86ba0606a4f86d5e7d903af81a3
John Engdal <john@walkbase.com>
2026-09-06 21:41:30 +0300

    faster mouse scrolling

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -24,6 +24,9 @@ vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.root_spec = { "cwd" }
 
 vim.g.snacks_animate = false
+
+vim.opt.mousescroll = "ver:5,hor:6"
+
 if vim.g.neovide == true then
   vim.g.neovide_input_use_logo = 1
   vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
